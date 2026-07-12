5bfb27c9c74f2747410a975de56402bbd7ca9f4b
Krrish Ganesh <43213544+masterkris@users.noreply.github.com>
2026-07-12 13:03:13 -0500

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+
+        # this is odd, even center question
+        # palindrome can either have odd center (1 char) or even center (between 2 chars)
+    
+        def countPalindrome(s, l, r):
+
+    # if l > 0 and r < len(s), and equal (palindrome condition)
+            total = 0
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                total += 1
+                l -= 1 # expanding outward from center
+                r += 1
+            return total
+
+        # now start this function
+        
+        res = 0
+
+        for i in range(len(s)): # we're doing this for each index in the string btw
+            res += countPalindrome(s, i, i) # odd     # abc
+            res += countPalindrome(s, i, i + 1) # even  # aaaa
+        return res
+
+        # palindrome function
+
+   
