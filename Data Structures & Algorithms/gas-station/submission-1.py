f7fba7b5b4f83d272477694319d2b16912b2c9de
Krrish Ganesh <43213544+masterkris@users.noreply.github.com>
2026-07-16 17:30:21 -0500

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        # sum of cost should not be more than gas
+
+        if sum(cost) > sum(gas):
+            return -1
+
+        total = 0  # current amount of gas
+        res = 0   # starting index
+
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0: # current starting point can't work
+                total = 0 # reset and try again
+                res = i + 1 # set next station as new start
+            
+        return res
